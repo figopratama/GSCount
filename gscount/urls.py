48e19c8a2842a23a0d689b0c874106d3d2c9371b
figopratama <socialmedia.figo@gmail.com>
2026-06-12 10:43:15 +0700

    fix: redirect root to home

--- gscount/urls.py
+++ gscount/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 from . import views
 from .views import style, team, partner
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', lambda request: redirect('home'), name='index'),
     path('', include('count.urls')),
     path('home/', views.home, name='home'),
     path('team/', views.team, name='team'),
